da28c116efb9a98c080d826266c091a6c10c43b4
Diana <dsold7619@gmail.com>
2023-10-03 12:32:32 +0300

    creat test for page

--- src/test/java/ru/sold/tests/SwagLabTest.java
+++ src/test/java/ru/sold/tests/SwagLabTest.java
@@ -1,28 +1,20 @@
 package ru.sold.tests;
 
-import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import ru.sold.pages.ProductsPage;
+import ru.sold.pages.StartPage;
 
 
 public class SwagLabTest extends BaseTests {
 
     @Test
     public void test() {
-        WebElement userName = driver.findElement(By.xpath("//input[@id='user-name']"));
-        userName.click();
-        userName.sendKeys("standard_user");
+        StartPage startPage = new StartPage(driver);
+        startPage.inputUserName();
+        startPage.inputUserPassword();
+        startPage.buttonLogin();
 
-        WebElement userPassword = driver.findElement(By.xpath("//input[@id='password']"));
-        userPassword.click();
-        userPassword.sendKeys("secret_sauce");
-
-        WebElement userLogin = driver.findElement(By.xpath("//input[@id='login-button']"));
-        userLogin.click();
-        String url = driver.getCurrentUrl();
-        String errorMessage = "Страничка не загрузилась";
-        Assert.assertEquals(errorMessage, "https://www.saucedemo.com/inventory.html", url);
+        new ProductsPage(driver);
     }
 }
 
