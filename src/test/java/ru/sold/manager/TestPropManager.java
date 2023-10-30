0ee89576987e0d852dc5c1ace07d4ad8f079cc9c
Diana <dsold7619@gmail.com>
2023-10-30 11:39:36 +0300

    add manager for properties

---
+++ src/test/java/ru/sold/manager/TestPropManager.java
@@ -0,0 +1,38 @@
+package ru.sold.manager;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestPropManager {
+
+    public static final Properties PROPERTIES = new Properties();
+    private static TestPropManager instance = null;
+
+    private TestPropManager() {
+        loadApplicationProperties();
+    }
+
+    public static TestPropManager getInstance() {
+        if (instance == null) {
+            instance = new TestPropManager();
+        }
+        return instance;
+    }
+
+    private void loadApplicationProperties() {
+        try (FileInputStream fis = new FileInputStream("src/test/resources/" + System.getProperty("propFile", "application") + ".properties")) {
+            PROPERTIES.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getProperty(String key, String defaultValue) {
+        return PROPERTIES.getProperty(key, defaultValue);
+    }
+
+    public String getProperty(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+}
--- src/test/java/ru/sold/tests/BaseTests.java
+++ src/test/java/ru/sold/tests/BaseTests.java
@@ -6,24 +6,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.sold.manager.TestPropManager;
+import ru.sold.utils.PropsConst;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static ru.sold.utils.PropsConst.BASE_URL;
 
 public class BaseTests {
     protected WebDriver driver;
+    private final TestPropManager propManager = TestPropManager.getInstance();
     protected WebDriverWait wait;
 
     @Before
     public void before() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", propManager.getProperty(PropsConst.PATH_CHROME_DRIVER_WINDOWS));
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(10, SECONDS);
-        driver.manage().timeouts().implicitlyWait(10, SECONDS);
-        wait = new WebDriverWait(driver, 10, 2000);
-        driver.navigate().to("https://www.saucedemo.com/");
+        driver.manage().timeouts().pageLoadTimeout(Integer.parseInt(propManager.getProperty(PropsConst.PAGE_LOAD_TIMEOUT)), SECONDS);
+        driver.manage().timeouts().implicitlyWait(Integer.parseInt(propManager.getProperty(PropsConst.IMPLICITLY_WAIT)), SECONDS);
+        wait = new WebDriverWait(driver, 5, 2000);
+        driver.navigate().to(propManager.getProperty(BASE_URL));
     }
 
     @After
