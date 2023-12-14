e0ea377d147bbd7bd83c5c7b48c2ddd347612346
Diana <dsold7619@gmail.com>
2023-12-14 13:04:18 +0300

    add CartItems to the CartPage

--- src/test/java/ru/sold/pages/CartPage.java
+++ src/test/java/ru/sold/pages/CartPage.java
@@ -1,13 +1,19 @@
 package ru.sold.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.sold.manager.DriverManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartPage extends BasePage {
     private final WebDriver driver;
     private final String pageUrl = "https://www.saucedemo.com/cart.html";
+
+    private final String cartItemBaseXPath = "//div[@class='cart_item']";
     @FindBy(xpath = "//div[@id='cart_contents_container']")
     private WebElement cartContainer;
 
@@ -19,6 +25,8 @@ public class CartPage extends BasePage {
 
     private final HeaderPanel headerPanel = new HeaderPanel();
 
+    private final List<CartItem> cartItems = new ArrayList<>();
+
     public CartPage() {
         driver = DriverManager.getInstance().getDriver();
         checkPageInit();
@@ -27,6 +35,18 @@ public class CartPage extends BasePage {
     public void checkPageInit() {
         if (!pageUrl.equals(driver.getCurrentUrl()) & !cartContainer.isDisplayed()) {
             throw new AssertionError("Error loading page");
+        }
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    private void createCarItems() {
+        int count = DriverManager.getInstance().getDriver().findElements(By.xpath(cartItemBaseXPath)).size();
+        for (int index = 1; index <= count; index++) {
+            CartItem cartItem = new CartItem(driver, cartItemBaseXPath, index);
+            cartItems.add(cartItem);
         }
     }
 }
