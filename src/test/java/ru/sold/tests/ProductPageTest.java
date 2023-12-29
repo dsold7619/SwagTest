907c2af0fe0141d79106d762ac21d0cc18dff214
Diana <dsold7619@gmail.com>
2023-12-29 14:59:26 +0300

    add test to ProductPageTest

--- src/test/java/ru/sold/tests/ProductPageTest.java
+++ src/test/java/ru/sold/tests/ProductPageTest.java
@@ -2,10 +2,9 @@ package ru.sold.tests;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.sold.pages.CartPage;
-import ru.sold.pages.ProductsPage;
-import ru.sold.pages.SideBar;
-import ru.sold.pages.StartPage;
+import ru.sold.pages.*;
+
+import java.util.List;
 
 public class ProductPageTest extends BaseTests {
 
@@ -19,7 +18,7 @@ public class ProductPageTest extends BaseTests {
     public void checkTitleForProductItem() {
         ProductsPage productsPage = new StartPage().navigateToProductsPage();
         String actualTitle = productsPage.getFirstProductItem().getTitle();
-        Assert.assertEquals("Product name in the cart doesn't match selected","Sauce Labs Backpack", actualTitle);
+        Assert.assertEquals("Product name in the cart doesn't match selected", "Sauce Labs Backpack", actualTitle);
     }
 
     @Test
@@ -39,6 +38,33 @@ public class ProductPageTest extends BaseTests {
         Assert.assertEquals(1, cartPage.getCartItems().size());
         productsPage = cartPage.clickContinueShoppingButton();
         String textAfter = productsPage.getFirstProductItem().getButtonText();
-        Assert.assertEquals("Button name did not change after adding the product to the cart","Remove", textAfter);
+        Assert.assertEquals("Button name did not change after adding the product to the cart", "Remove", textAfter);
+    }
+
+    @Test
+    public void checkProductItemButtonRemove() {
+        ProductsPage productsPage = new StartPage().navigateToProductsPage();
+        productsPage.getProductItems().stream()
+                .limit(3)
+                .forEach(ProductItem::clickAddToCartButton);
+
+        CartPage cartPage = productsPage.getHeaderPanel().clickCartBtn();
+
+        productsPage = cartPage.clickContinueShoppingButton();
+        List<ProductItem> productItems = new java.util.ArrayList<>(productsPage.getProductItems().stream()
+                .filter(productItem -> productItem.getButtonText().equals("Remove"))
+                .toList());
+        productItems.get(0).clickRemoveButton();
+        productItems.remove(0);
+        List<String> productItemTitles = productItems.stream()
+                .map(ProductItem::getTitle)
+                .toList();
+
+        cartPage = productsPage.getHeaderPanel().clickCartBtn();
+        List<String> cartItemTitles = cartPage.getCartItems().stream()
+                .map(CartItem::getTitle)
+                .toList();
+
+        Assert.assertEquals("Nomenclature from the ProductsPage corresponds with the CartPage", productItemTitles, cartItemTitles);
     }
 }
